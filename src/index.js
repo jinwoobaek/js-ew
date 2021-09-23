6a707a367ec151106a11e52d2e87d7a0eae542f7
jinwoo_baek <jinwoo_baek@sdplex.com>
2021-09-23 16:26:22 +0900

    MongoDB 스키마 작성

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 require('dotenv').config();
+
 const db = require('./db');
+const models = require('./models');
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-// test data
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Jinwoo' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
-];
 
 // 그래프QL 스키마 언어로 스키마를 구성
 const typeDefs = gql`
@@ -34,20 +29,19 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello World!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     },
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: 'Adam Scott',
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     },
   },
 };
